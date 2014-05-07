0bbdefe7531d1085e52867a2cd9427e6d6c11991
falkjm/Statistik-f-r-Wissenschaftler <Falkj@students.uni-marburg.de>
2014-05-07 10:11:14 +0200

    Hausaufgabe05 Name/Date Change

--- hausaufgabe05.R
+++ hausaufgabe05.R
@@ -1,6 +1,6 @@
 # Hausaufgabe 05
-# Phillip Alday <phillip.alday@staff.uni-marburg.de>
-# 2014-05-02
+# Michael Falk <Falkj@students.uni-marburg.de>
+# 2014-05-07
 # Dieses Werk ist lizenziert unter einer CC-BY-NC-SA Lizenz.
 
 # Sie sollten die Datei auch in Ihren Ordner kopieren und einen Commit machen, 
