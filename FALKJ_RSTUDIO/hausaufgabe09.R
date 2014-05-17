ca0c7838034d2df24f4ed3c8b8eabbf814aa98a4
falkjm/Statistik-f-r-Wissenschaftler <Falkj@students.uni-marburg.de>
2014-05-17 20:45:23 +0200

    Hausaufgabe09 Two.Sample/Welch/Shapiro

--- FALKJ_RSTUDIO/hausaufgabe09.R
+++ FALKJ_RSTUDIO/hausaufgabe09.R
@@ -119,7 +119,13 @@ The Pr(>F) is 0.3656 which is greater than 0.05.  This means that the variablity
 # Variante ähnliche bzw. (fast) gleiche Ergebnisse liefern. Ist das hier der
 # Fall?
 # two.sample <- CODE_HIER
+two.sample<-t.test(subj.1,subj.2,var.equal=TRUE)
+print(two.sample)
+
+
 # welch <- CODE_HIER
+welch<-t.test(subj.1,subj.2)
+print(welch)
 
 # print(two.sample)
 # print(welch)
@@ -129,25 +135,47 @@ The Pr(>F) is 0.3656 which is greater than 0.05.  This means that the variablity
 # beiden Testverfahren gab. Wenn die Varianz homogen war, sollten wir keinen
 # Unterschied sehen:
 # t.diff <- welch$statistic - two.sample$statistic
+t.diff<-welch$statistic-two.sample$statistic
+
 # print(paste("Die Differenz zwischen den beiden t-Werten ist",t.diff,"."))
+print(paste("Die Differenz zwischen den beiden t-Werten ist",t.diff,"."))
 
 # Sind die Daten normal verteilt? Wir berechnen Sie den Shapiro Test für erste Versuchsperson:
 # shapiro <- shapiro.test(rt[rt$subj==1,"RT"])
+shapiro<-shapiro.test(rt[rt$subj==1,"RT"])
 # 
 # print(shapiro)
+print(shapiro)
 
 # Wir können auch "Entscheidungen" im Code treffen. Die Syntax dafür ist wie
 # folgt -- die runden und geschweiften Klammern sind alle sehr wichtig!
-# if (shapiro$p.value > 0.05){
-#   print("Shapiro's test insignikant, die Daten sind normal verteilt.")
-# }else{
-#   print("Shapiro's test signikant, die Daten sind nicht normal verteilt.")
-# }
+if (shapiro$p.value > 0.05){
+   print("Shapiro's test insignikant, die Daten sind normal verteilt.")
+ }else{
+   print("Shapiro's test signikant, die Daten sind nicht normal verteilt.")
+ }
+
+
+if(shapiro$p.value > 0.05){
+  print(Shapiro's test insignikant, die Daten sind normal verteilt.")
+}else{
+print("Shapiro's test signikant, die Daten sind nicht normal verteilt.")
+}
 
 # Berechnen Sie Shapiro's Test für die andere Versuchsperson und drücken Sie mit
 # einem if-Block aus, ob die Daten normal verteilt sind.
 
 # CODE_HIER
+shapiro<-shapiro.test(rt[rt$subj==2,"RT"])
+print(shapiro)
+
+if (shapiro$p.value > 0.05){
+   print("Shapiro's test insignikant, die Daten sind normal verteilt.")
+ }else{
+   print("Shapiro's test signikant, die Daten sind nicht normal verteilt.")
+ }
+
+
 
 # Wir haben auch Transformationen bei schiefen Datenverteilungen angesprochen.
 # Die logaritmische Verteilung ist ziemlich beliebt bei Reaktionszeitsdaten.
